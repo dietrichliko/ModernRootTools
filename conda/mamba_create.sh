f0c37f582932c6521575e6019f28bcd6d7c7c7f1
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2023-03-28 09:49:38 +0200

    New start

--- conda/mamba_create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-if [ "$CONDA_DEFAULT_ENV" != "my-base" ]
-then
-    conda activate my-base
-fi
-
-mamba create -y -n mrt -c conda-forge --file=pkgs-root626-py310.txt
--- conda/remove_envs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-
-for env in mrtools-root626-py310
-do
-    conda remove -y -n $env --all
-done
